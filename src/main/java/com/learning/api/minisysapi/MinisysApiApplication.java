5f1b0180db66723227d7730cab76d7898061f6a3
Arlindo Silva <arlindovicente@gmail.com>
2023-11-12 19:09:50 -0300

    Initial commit

---
+++ src/main/java/com/learning/api/minisysapi/MinisysApiApplication.java
@@ -0,0 +1,13 @@
+package com.learning.api.minisysapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinisysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinisysApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/api/minisysapi/MinisysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.api.minisysapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinisysApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
